cdc239d0348df0e2c0fafe3d10191147c6b95916
tomm <somjeth@gmail.com>
2018-10-06 00:04:27 +0700

    edit some texts

--- AtmSimulation/src/main/java/com/mydomain/model/Atm.java
+++ AtmSimulation/src/main/java/com/mydomain/model/Atm.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Atm {
-	
+	//
 	public static final int DEFAULT_BANKNOTE_QUANTITY = 100;
 	private List<BanknoteLot> banknoteLots;
 	
